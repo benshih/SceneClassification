a0dc6f244d61e73ce458eb4c60bf6b470dbd6348
Ben Shih <benshih@cmu.edu>
2013-10-03 23:06:17 -0400

    implemented SURF features - instead of choosing alpha random points we now take the strongest SURF points available and buffer with random points if there aren't enough SURF points.

--- custom/distanceToSet.m
+++ custom/distanceToSet.m
@@ -17,14 +17,51 @@ function [ histInter ] = distanceToSet( wordHist, histograms)
 
 % chi2 distance
 histogramDim = size(histograms);
-%histInter = sum(chi2pdf(repmat(wordHist, 1, histogramDim(2)), histograms));
-%histInter = sum((repmat(wordHist,1,histogramDim(2))-histograms).^2 ./ (repmat(wordHist,1,histogramDim(2)) + histograms))/2;
+%histInter = -sum(chi2pdf(repmat(wordHist, 1, histogramDim(2)), histograms));
+%histInter = -sum((repmat(wordHist,1,histogramDim(2))-histograms).^2 ./ (repmat(wordHist,1,histogramDim(2)) + histograms))/2;
+% % confusion matrix:
+% %     20     0     0     0     0     0     0     0
+% %     20     0     0     0     0     0     0     0
+% %     20     0     0     0     0     0     0     0
+% %     20     0     0     0     0     0     0     0
+% %     20     0     0     0     0     0     0     0
+% %     20     0     0     0     0     0     0     0
+% %     20     0     0     0     0     0     0     0
+% %     20     0     0     0     0     0     0     0
+% % 
+% % accuracy = 0.125
+% % Elapsed time is 860.394593 seconds.
+
 
 % L1 distance - Euclidean
-%histInter = max(abs(repmat(wordHist, 1, histogramDim(2))-histograms));
+histInter = -max(abs(repmat(wordHist, 1, histogramDim(2))-histograms));
+% % confusion matrix:
+% %      4     3     0     0     7     1     2     3
+% %      0    13     1     3     1     1     1     0
+% %      0     3    11     0     1     0     3     2
+% %      0     6     5     5     0     2     1     1
+% %      4     1     0     2    10     0     2     1
+% %      0     5     4     4     1     5     0     1
+% %      1     3     1     2     3     2     5     3
+% %      2     0     1     2     8     1     3     3
+% % 
+% % accuracy = 0.35
+% % Elapsed time is 850.780331 seconds.
 
 % euclidean using pdist
-histInter = pdist2(wordHist', histograms', 'hamming');
+%histInter = -pdist2(wordHist', histograms', 'hamming');
+% % confusion matrix:
+% %      2     0     0     0     2     0     0    16
+% %      4     0     0     0     8     0     0     8
+% %      7     0     0     0     9     0     0     4
+% %     10     0     0     0     5     0     0     5
+% %      2     0     0     0     3     0     0    15
+% %      1     0     0     0    12     0     0     7
+% %      2     0     0     0    11     0     0     7
+% %      0     0     0     0     0     0     0    20
+% % 
+% % accuracy = 0.15625
+% % Elapsed time is 893.446981 seconds.
 
 end
 
--- custom/getFilterBankAndDictionary.m
+++ custom/getFilterBankAndDictionary.m
@@ -7,8 +7,8 @@ function [ filterBank, dictionary ] = getFilterBankAndDictionary( imPaths )
 
 % Number of visual words in dictionary: K is [100, 300].
 K = 200;
-% Number of random pixels to take the response of: alpha is [50, 150].
-alpha = 100;
+% Number of strongest features to take.
+alpha = 20;
 
 
 % Load each file by iterating through the cell array of strings containing
@@ -20,13 +20,22 @@ alphaPixelImg = zeros(length(imPaths) * alpha, 1, 3);
 for Ti = 1:length(imPaths)
     % Read and store the image.
     img = imread(imPaths{Ti});
-    %imList{i} = img;
     
-    % Generate a random permutation of the pixels to select for their
-    % responses.
+    % Identify the SURF features, take the strongest alpha points. 
+    imgDim = size(img);
+    
     numPixel = numel(img(:,:,1));
     pixels = randperm(numPixel, alpha);
     
+    points = detectSURFFeatures(img(:,:,1));
+    strongestP = points.selectStrongest(alpha);
+    pixelCoord = round(strongestP.Location)';
+    if not(isempty(pixelCoord))
+        pixels(1:length(pixelCoord)) = pixelCoord(2,:).*imgDim(2) + pixelCoord(1,:);
+    end
+
+    %Ti
+
     % Apply all of the filters to the alpha random pixels selected using
     % randperm. The alpha random pixels are selected in one color channel
     % of the image, then the corresponding pixels from all three of the RGB
@@ -34,7 +43,7 @@ for Ti = 1:length(imPaths)
     % still has 3 RGB channels. Then, this new sub-image is passed into the
     % extractFilterResponses function.
     R = img(:,:,1);
-    G = img(:,:,2);
+    G = img(:,:,2); 
     B = img(:,:,3);
     RCol = R(:);
     GCol = G(:);
@@ -42,6 +51,7 @@ for Ti = 1:length(imPaths)
     RAlphaPixels = RCol(pixels);
     GAlphaPixels = GCol(pixels);
     BAlphaPixels = BCol(pixels);
+
     alphaPixelImg(1+alpha*(Ti-1):Ti*alpha, 1) = RAlphaPixels;
     alphaPixelImg(1+alpha*(Ti-1):Ti*alpha, 2) = GAlphaPixels;
     alphaPixelImg(1+alpha*(Ti-1):Ti*alpha, 3) = BAlphaPixels;
